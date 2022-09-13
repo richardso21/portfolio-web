81c1e64b5bdd04f6bb1a0f64a0d08ad30c17d615
Richard So <richardso2021@gmail.com>
2022-09-12 22:09:09 -0400

    switch prismic resume link for github resume

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,18 +1,16 @@
-import { asLink } from '@prismicio/helpers';
-import { GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
-import { createClient } from '../prismicio';
+import { NextPage } from 'next';
 
-export const getStaticProps: GetStaticProps = async (context) => {
-  const client = createClient(context.previewData);
-  // query for pdf link
-  const pdfItem = client.getByUID('pdf', 'resume');
-  const pdfLink = asLink((await pdfItem).data.pdfLink);
-  return {
-    props: { pdfLink },
-    revalidate: 60,
-  };
-};
+// export const getStaticProps: GetStaticProps = async (context) => {
+//   const client = createClient(context.previewData);
+//   // query for pdf link
+//   const pdfItem = client.getByUID('pdf', 'resume');
+//   const pdfLink = asLink((await pdfItem).data.pdfLink);
+//   return {
+//     props: { pdfLink },
+//     revalidate: 60,
+//   };
+// };
 
 const Resume: NextPage = ({ pdfLink }: any) => (
   <>
@@ -20,7 +18,11 @@ const Resume: NextPage = ({ pdfLink }: any) => (
       <title>Resume | Richard So</title>
     </Head>
     <div className="h-screen w-screen">
-      <iframe className="h-full w-full" src={pdfLink} />
+      {/* <iframe className="h-full w-full" src={pdfLink} /> */}
+      <iframe
+        className="h-full w-full"
+        src="https://cdn.jsdelivr.net/gh/richardso21/resume/RSOresume.pdf"
+      />
     </div>
   </>
 );
